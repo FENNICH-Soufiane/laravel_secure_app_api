5c7b1f31d903745e24d0284be3bc88bbb7c17783
FENNICH-Soufiane <fennich0011soufiane@gmail.com>
2023-12-31 22:18:52 +0100

    register&login

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    //
+    public function login(LoginRequest $request) {
+        
+        $credentials = [
+            "email"=> $request->email,
+            "password"=> $request->password
+        ];
+        if(auth()->attempt($credentials)) {
+            // $user = Auth::user();
+            $user = auth()->user();
+
+            // delete last token if exist
+            $user->tokens()->delete();
+            $success['token'] = $user->createToken(request()->userAgent())->plainTextToken;
+            $success['name'] = $user->first_name;
+            $success['success'] = true;
+            return response()->json($success, 200);
+        } else {
+          return response()->json(['error'=> 'Unauthorised'], 401);
+          // is the same
+          // return response()->json(['error'=> __('auth.Unauthorised')],401);
+        }
+
+        
+    }
+}
---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Http\Requests\Auth\RegisterRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+// 
+
+// use Hash;
+
+class RegisterController extends Controller
+{
+    //
+    // Function for Register
+    public function register(RegisterRequest $request) {
+        // For validate data of registration
+        $newuser = $request->validated();
+
+        $newuser['password'] = Hash::make($newuser['password']);
+        $newuser['role'] = 'user';
+        $newuser['status'] = 'active';
+
+        $user = User::create($newuser);
+
+        $success['token'] = $user->createToken('user', ['app:all'])->plainTextToken;
+        $success['name'] = $user->first_name;
+        $success['success'] = true;
+
+        return response()->json($success, 200);
+    }
+}
